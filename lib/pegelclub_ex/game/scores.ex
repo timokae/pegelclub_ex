c0c43e37b2106bd2f29cbadfb6cc971518c77338
timokae <timo2503k@gmail.com>
2022-09-07 18:01:07 +0200

    Own function to calculate each penalty euro value

--- lib/pegelclub_ex/game/scores.ex
+++ lib/pegelclub_ex/game/scores.ex
@@ -3,6 +3,9 @@ defmodule PegelclubEx.Game.Scores do
   alias PegelclubEx.Repo
   alias PegelclubEx.Game.{Players, Score}
 
+  @delay_limit 10_000
+  @early_leave_limit 10_000
+
   @penalty_delay 100
   @penalty_early_leave 100
 
@@ -48,19 +51,41 @@ defmodule PegelclubEx.Game.Scores do
 
   def penalty_sum(score) do
     [
-      score.penalty_25 * @penalty_25,
-      score.penalty_50 * @penalty_50,
-      score.penalty_75 * @penalty_75,
-      score.penalty_100 * @penalty_100,
-      score.penalty_125 * @penalty_125,
-      score.penalty_150 * @penalty_150,
-      score.penalty_175 * @penalty_175,
-      score.penalty_500 * @penalty_500,
-      score.delay * @penalty_delay,
-      score.early_leave * @penalty_early_leave,
-      score.other * 100
+      penalty_25(score),
+      penalty_50(score),
+      penalty_75(score),
+      penalty_100(score),
+      penalty_125(score),
+      penalty_150(score),
+      penalty_175(score),
+      penalty_500(score),
+      penalty_delay(score),
+      penalty_early_leave(score),
+      penalty_other(score)
     ] |> Enum.sum()
   end
+
+  def penalty_sum(score, pudel_king_value) do
+    penalty_sum(score) + penalty_pudel(score, pudel_king_value)
+  end
+
+  def penalty_delay(%Score{} = score) do
+    Enum.min([score.delay * @penalty_delay, @delay_limit])
+  end
+
+  def penalty_early_leave(%Score{} = score) do
+    Enum.min([score.early_leave * @penalty_early_leave, @early_leave_limit])
+  end
+
+  def penalty_25(%Score{} = score), do: score.penalty_25 * @penalty_25
+  def penalty_50(%Score{} = score), do: score.penalty_50 * @penalty_50
+  def penalty_75(%Score{} = score), do: score.penalty_75 * @penalty_75
+  def penalty_100(%Score{} = score), do: score.penalty_100 * @penalty_100
+  def penalty_125(%Score{} = score), do: score.penalty_125 * @penalty_125
+  def penalty_150(%Score{} = score), do: score.penalty_150 * @penalty_150
+  def penalty_175(%Score{} = score), do: score.penalty_175 * @penalty_175
+  def penalty_500(%Score{} = score), do: score.penalty_500 * @penalty_500
+  def penalty_other(%Score{} = score), do: score.other * 100
 
   def penalty_pudel(%Score{} = score, pudel_king_value) do
     result = score.penalty_pudel * @penalty_pudel;
