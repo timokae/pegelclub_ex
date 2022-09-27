2d0bc3d9ab1dd671d4e12ce73e12bf506a52b75a
timokae <timo2503k@gmail.com>
2022-09-27 11:05:52 +0200

    Add redirect to new player

--- lib/pegelclub_ex_web/controllers/player_controller.ex
+++ lib/pegelclub_ex_web/controllers/player_controller.ex
@@ -9,20 +9,33 @@ defmodule PegelclubExWeb.PlayerController do
     render(conn, "index.html", players: players)
   end
 
-  def new(conn, _params) do
+  def new(conn, %{"redirect" => redirect_url}) do
     changeset = Game.change_player(%Player{}, %{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset, redirect_url: redirect_url)
   end
 
-  def create(conn, %{"player" => player_params}) do
+  def new(conn, _params) do
+    redirect_url = PegelclubExWeb.Router.Helpers.player_path(conn, :index)
+    changeset = Game.change_player(%Player{}, %{})
+    render(conn, "new.html", changeset: changeset, redirect_url: redirect_url)
+  end
+
+  def create(conn, %{"player" => player_params} = params) do
+    redirect_to =
+      if params["redirect"] do
+        URI.decode_www_form(params["redirect"])
+      else
+        Routes.player_path(conn, :index)
+      end
+
     case Game.create_player(player_params) do
       {:ok, _player} ->
         conn
         |> put_flash(:info, "Player created successfully.")
-        |> redirect(to: Routes.player_path(conn, :index))
+        |> redirect(to: redirect_to)
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", changeset: changeset, redirect: URI.encode_www_form(redirect_to))
     end
   end
 
